512146a7dc74a1b2d79ae76f3b50be419ababdf0
Kamila <kamilochkagilmanova@gmail.com>
2026-02-01 10:56:55 +0300

    feat: добавлена логика условий в JS и стилизована панель инструментов

--- js/task.js
+++ js/task.js
@@ -3,9 +3,15 @@ let b = 9
 
 let sum = a + b 
 let diff = a - b
+let mult = a * b;
+let div = a / b; 
 
-console.log(sum, diff)
-console.log(diff)
+let firstName = "Камила";
+let lastName = "Гильманова";
+let fullName = firstName + " " + lastName;
+
+console.log(sum, diff, mult, div);
+console.log(fullName);
 
 // Урок 2: Операторы сравнения и логические операторы
 
@@ -19,7 +25,44 @@ if (title === "") {
 
 let tasks = 5
 if (tasks === 0) {
-    console.log("Нет задач для выполнения");
-} else if (tasks > 3) {
+    console.log("Список пуст");
+} else if (tasks >= 1 && tasks <= 3) {
+    console.log("Немного задач");
+} else {
     console.log("Много задач");
+}
+
+let isCompleted = false;
+if (isCompleted) {
+    console.log("Задача выполнена");
+} else {
+    console.log("Задача ещё в работе");
+}
+
+let urgent = true;
+if (tasks > 0 && urgent) {
+    console.log("Есть срочные задачи");
+} else if (tasks > 0 && !urgent) {
+    console.log("Задачи есть, но они не срочные");
+} else {
+    console.log("Все задачи завершены");
+}
+
+let isAdmin = false;
+let isModerator = true;
+if (isAdmin || isModerator) {
+    console.log("Доступ разрешён");
+} else {
+    console.log("Доступ запрещён");
+}
+
+let amount = 2500; 
+if (amount === 0) {
+    console.log("Корзина пуста");
+} else if (amount < 1000) {
+    console.log("Скидка не применяется");
+} else if (amount >= 1000 && amount < 5000) {
+    console.log("Скидка 5%");
+} else if (amount >= 5000) {
+    console.log("Скидка 10%");
 }
