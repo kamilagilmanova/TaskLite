fb1fb70b4d6e8f098c0850e29fa8d840bf6471ba
Kamila <kamilochkagilmanova@gmail.com>
2026-03-01 17:55:33 +0300

    Добавление новых задач

--- js/main.js
+++ js/main.js
@@ -6,33 +6,33 @@ const searchInput = document.querySelector(".toolbar__search");
 const footer = document.querySelector(".footer-controls");
 const sortSelect = document.querySelector(".toolbar__sort");
 
-function renderTask() {
-  container.innerHTML = "";
+function renderTask(task) {
+  // container.innerHTML = "";
 
-  tasks.forEach((task) => {
-    const item = document.createElement("div");
-    item.classList.add("task");
+  // tasks.forEach((task) => {
+  const item = document.createElement("div");
+  item.classList.add("task");
 
-    const content = document.createElement("div");
-    content.classList.add("task__content");
+  const content = document.createElement("div");
+  content.classList.add("task__content");
 
-    const title = document.createElement("div");
-    title.classList.add("task__title");
-    title.textContent = task.text;
+  const title = document.createElement("div");
+  title.classList.add("task__title");
+  title.textContent = task.text;
 
-    const meta = document.createElement("div");
-    meta.classList.add("task__meta");
-    meta.textContent = task.date;
+  const meta = document.createElement("div");
+  meta.classList.add("task__meta");
+  meta.textContent = task.date;
 
-    content.append(title, meta);
+  content.append(title, meta);
 
-    const actions = document.createElement("div");
-    actions.classList.add("task__actions");
+  const actions = document.createElement("div");
+  actions.classList.add("task__actions");
 
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("task__action", "task__action--edit");
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("task__action", "task__action--edit");
 
-    editBtn.innerHTML = `<svg
+  editBtn.innerHTML = `<svg
               class="task__icon"
               viewbox="0 0 24 24"
               width="14"
@@ -48,17 +48,17 @@ function renderTask() {
                 d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
               />
             </svg>`;
-    editBtn.addEventListener("click", () => {
-      const newText = prompt("Изменить задачу:", task.text);
-      if (newText && newText.trim() !== "") {
-        task.text = newText.trim();
-        renderTask();
-      }
-    });
+  editBtn.addEventListener("click", () => {
+    const newText = prompt("Изменить задачу:", task.text);
+    if (newText && newText.trim() !== "") {
+      task.text = newText.trim();
+      renderAll();
+    }
+  });
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("task__action", "task__button--delete");
-    deleteBtn.innerHTML = `<svg
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("task__action", "task__button--delete");
+  deleteBtn.innerHTML = `<svg
               class="task__icon"
               viewbox="0 0 24 24"
               width="14 "
@@ -76,27 +76,30 @@ function renderTask() {
               <path d="M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2" />
             </svg>`;
 
-    deleteBtn.addEventListener("click", () => {
-      const index = tasks.indexOf(task);
+  deleteBtn.addEventListener("click", () => {
+    const index = tasks.indexOf(task);
 
-      tasks.splice(index, 1);
+    tasks.splice(index, 1);
 
-      renderTask();
-    });
-
-    item.addEventListener("click", () => {
-      task.done = !task.done;
-    });
-
-    actions.append(editBtn, deleteBtn);
-    item.append(content, actions);
-
-    if (task.done) {
-      item.classList.add("task--done");
-    }
-
-    container.append(item);
+    renderAll();
   });
+
+  item.addEventListener("click", (event) => {
+    if (event.target.closest(".task__action")) return;
+    task.done = !task.done;
+    renderAll();
+  });
+
+  actions.append(editBtn, deleteBtn);
+  item.append(content, actions);
+
+  if (task.done) {
+    item.classList.add("task--done");
+  }
+
+  return item;
+  //   container.append(item);
+  // });
 }
 
 const tabButtons = document.querySelectorAll(".tabs__item");
@@ -113,6 +116,47 @@ const tasks = [
     date: "Сегодня в 13:00",
     done: false,
   },
+  {
+    text: "Выполнить д/з",
+    date: "Сегодня в 11:00",
+    done: true,
+  },
+  {
+    text: "Убраться дома",
+    date: "Сегодня в 13:00",
+    done: false,
+  },
+  {
+    text: "Подготовить проект по веб-дизайну",
+    date: "Сегодня в 16:30",
+    done: false,
+  },
+  {
+    text: "Попрактиковать английскую лексику (IELTS)",
+    date: "Завтра в 10:00",
+    done: false,
+  },
+  {
+    text: "Испечь торт для Рейхан",
+    date: "Пятница в 18:00",
+    done: false,
+  },
+  {
+    text: "Посмотреть новую серию сериала",
+    date: "Вчера в 21:00",
+    done: true,
+  }
 ];
 
-renderTask();
+function renderAll() {
+  // container.innerHTML = "";
+
+  document.querySelectorAll(".task").forEach((t) => t.remove());
+
+  tasks.forEach((task) => {
+    const card = renderTask(task);
+    footer.before(card);
+  });
+}
+
+renderAll();
